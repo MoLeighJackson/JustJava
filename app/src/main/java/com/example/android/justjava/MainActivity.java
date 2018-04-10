0e00b3888a508d9a57db3eb3fe00c2a5f3d3e90a
MoLeighJackson <moleighjackson@gmail.com>
2018-04-09 21:45:29 -0400

    Added check box and option to add whipped cream to coffee

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.justjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
@@ -23,43 +24,65 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
-
     /**
      * This method is called when the plus (+) button is clicked.
      */
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
-
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String message = "Item count " + quantity;
-        displayMessage(message);
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.add_whipped_cream_checkbox);
+        boolean addWhippedCream = whippedCreamCheckBox.isChecked();
+        /**
+         * message to test if add whipped cream logic is working
+         * Log.v("MainActivity", "Add whipped cream: " + addWhippedCream);
+         */
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price, addWhippedCream);
+        displayMessage(priceMessage);
     }
-
+    /**
+     * Calculates the price of the order.
+     *
+     * @return total price of order
+     */
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+    /**
+     * Declares: create order summary
+     *
+     * @param price of the order
+     * @return text summary
+     */
+    private String createOrderSummary(int price, boolean whippedCream){
+        String priceMessage = "Name: Customer 1";
+        priceMessage += "\nAdd whipped cream?" + whippedCream;
+        priceMessage = priceMessage + "\nNumber of coffees: " + quantity;
+        priceMessage = priceMessage + "\nOrder total: $" + price;
+        priceMessage = priceMessage + "\nThank You! \nYour order is on its way!";
+        return priceMessage;
+    }
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int numOfCoffees) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numOfCoffees);
     }
-
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
-    }
 }
